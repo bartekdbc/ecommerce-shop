9863f3520a91f69461b3a77fc3b1c2bbdfbabee2
Bartlomiej Debiec <116588815+bartekdbc@users.noreply.github.com>
2023-06-01 20:14:49 +0200

    Sidebar component added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React from "react";
+// import link
+import { Link } from "react-router-dom";
+// import icons
+import { IoMdArrowForward } from "react-icons/io";
+import { FiTrash2 } from "react-icons/fi";
+// import components
+import CartItem from "../components/CartItem";
+// import sidebar context
+import { SidebarContext } from "../contexts/SidebarContext";
 
 const Sidebar = () => {
+  const { isOpen, handleClose } = useContext(SidebarContext);
   return <div>Sidebar</div>;
 };
 
